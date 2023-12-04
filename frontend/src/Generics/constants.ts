1d517f3547d2c328e550bf5e0adf438a8ffc1194
DININDU THARUKA KALUARACHCHI <dinidutharuka@gmail.com>
2023-12-04 10:27:24 +0530

    make first page

---
+++ frontend/src/Generics/constants.ts
@@ -0,0 +1,9 @@
+export enum COLOURS {
+  MAIN_PAGE_YELLOW = "#ffec4b",
+  MAIN_PAGE_BLACK = 'black',
+  MAIN_PAGE_WHITE = 'white',
+}
+
+export enum SIZES {
+    MAIN_PAGE_BUTTON_BORDER_RADIOUS = 20
+}
--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,10 +1,75 @@
-import { Button } from "@chakra-ui/react"
-
+import { Button, Flex, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import restuarent from "../assets/images/restuarent.jpg";
+import { COLOURS, SIZES } from "../Generics/constants";
 
 const MainPage = () => {
   return (
-    <div><Button>Click</Button></div>
-  )
-}
+    <Flex width="100vw" height="100vh">
+      <Image src={restuarent} objectFit="cover" width="100vw" />
+      <Flex position="absolute" top="45vh" left="10vw">
+        <VStack alignItems="start">
+          <HStack>
+            <Text
+              fontWeight="bold"
+              fontSize={70}
+              color="white"
+              textShadow="1px 1px #ff0000"
+            >
+              Welcome to
+            </Text>
+            <Text
+              fontWeight="bold"
+              fontSize={70}
+              color={COLOURS.MAIN_PAGE_YELLOW}
+              textShadow="1px 1px #ff0000"
+            >
+              Restuarent
+            </Text>
+          </HStack>
+          <Text
+            textColor="white"
+            noOfLines={2}
+            width="40vw"
+            fontFamily="serif"
+            fontSize={20}
+            textShadow="1px 1px #ff0000"
+          >
+            Delight in exquisite flavors at our charming restaurant, where every
+            bite tells a unique story.
+          </Text>
+        </VStack>
+      </Flex>
 
-export default MainPage
+      <HStack position="absolute" top="70vh" left="50vw">
+        <Button
+          size="lg"
+          width="10vw"
+          borderRadius={SIZES.MAIN_PAGE_BUTTON_BORDER_RADIOUS}
+          variant="outline"
+          textColor={COLOURS.MAIN_PAGE_WHITE}
+          _hover={{
+            bg: COLOURS.MAIN_PAGE_YELLOW,
+            textColor: COLOURS.MAIN_PAGE_BLACK,
+          }}
+        >
+          Dining
+        </Button>
+        <Button
+          size="lg"
+          width="10vw"
+          borderRadius={SIZES.MAIN_PAGE_BUTTON_BORDER_RADIOUS}
+          variant="outline"
+          textColor={COLOURS.MAIN_PAGE_WHITE}
+          _hover={{
+            bg: COLOURS.MAIN_PAGE_YELLOW,
+            textColor: COLOURS.MAIN_PAGE_BLACK,
+          }}
+        >
+          Take away
+        </Button>
+      </HStack>
+    </Flex>
+  );
+};
+
+export default MainPage;
