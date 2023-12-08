f2dc83aa0a4b651d15786ff8760a081ed66e82de
DININDU THARUKA KALUARACHCHI <dinidutharuka@gmail.com>
2023-12-08 15:02:58 +0530

    insatll react hook form

--- frontend/src/services/api-client.ts
+++ frontend/src/services/api-client.ts
@@ -4,10 +4,10 @@ export const BASE_URL = 'http://127.0.0.1:8000'
 
 const axiosInstance =  axios.create({
     baseURL:`${BASE_URL}/api`,
-    headers:{
-        "Authorization":`JWT ${localStorage.getItem('access') || ''}`,
-        "Content-Type":'application/json',
-    }
+    // headers:{
+    //     "Authorization":`JWT ${localStorage.getItem('access') || ''}`,
+    //     "Content-Type":'application/json',
+    // }
    
    
 })
