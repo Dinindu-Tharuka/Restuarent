b1e3a7d78506e63d37373a91e2a5d7fe35751a8c
DININDU THARUKA KALUARACHCHI <dinidutharuka@gmail.com>
2023-12-10 22:00:39 +0530

    make category deletable

--- frontend/src/pages/Order/Componants/Categories/Form/ShowAllCategories.tsx
+++ frontend/src/pages/Order/Componants/Categories/Form/ShowAllCategories.tsx
@@ -1,8 +1,16 @@
-import { Container, Table, Tbody, Th, Tr } from "@chakra-ui/react";
+import {
+  Container,
+  Table,
+  Tbody,
+  Th,
+  Tr,
+} from "@chakra-ui/react";
 import useCategories from "../../../../../Hooks/Product/Category/useCategories";
+import ConfirmCategoryDelete from "./ConfirmCategoryDelete";
 
 const ShowAllCategories = () => {
   const { data: categories } = useCategories();
+
   return (
     <Container maxHeight="50vh" minHeight="60vh" overflow="auto">
       <Table>
@@ -10,7 +18,7 @@ const ShowAllCategories = () => {
           {categories?.map((category, index) => (
             <Tr key={index}>
               <Th>{category.title}</Th>
-              <Th></Th>
+              <Th><ConfirmCategoryDelete category={category}/></Th>
             </Tr>
           ))}
         </Tbody>
