e511880cdc67ac1cd7bd06f1b54980e987bbd09c
DININDU THARUKA KALUARACHCHI <dinidutharuka@gmail.com>
2023-12-18 19:04:37 +0530

    add serach categories in foods and beverages

--- frontend/src/pages/Order/Componants/Categories/Foods.tsx
+++ frontend/src/pages/Order/Componants/Categories/Foods.tsx
@@ -1,11 +1,13 @@
 import {
   Button,
+  Input,
   SimpleGrid,
   Text,
 } from "@chakra-ui/react";
 import { Category } from "../../../../Generics/interfaces";
 import { COLOURS, SIZES } from "../../../../Generics/constants";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 interface Props {
   categories: Category[] | undefined;
@@ -14,6 +16,7 @@ interface Props {
 
 const Foods = ({ categories, table }: Props) => {
   const isAvailble = categories?.filter((category) => category.is_food).length;
+  const [categoryFilter, setCategoryFilter] = useState("");
 
   const navigate = useNavigate();
 
@@ -24,9 +27,15 @@ const Foods = ({ categories, table }: Props) => {
   return (
     <>
       {isAvailble !== undefined && isAvailble > 0 ? (
+        <>
+        <Input
+            placeholder="Search"
+            onChange={(e) => setCategoryFilter(e.currentTarget.value)} marginBottom={5}
+          />
         <SimpleGrid columns={{ lg: 5, base: 3 }} spacing={2}>
           {categories
             ?.filter((category) => category.is_food)
+            .filter(category => category.title.startsWith(categoryFilter))
             .map((category, index) => (
               <Button
                 height={SIZES.CATEGORY_ITEM_HEIGHT}
@@ -41,6 +50,7 @@ const Foods = ({ categories, table }: Props) => {
               </Button>
             ))}
         </SimpleGrid>
+        </>
       ) : (
         <Text>No Foods</Text>
       )}
