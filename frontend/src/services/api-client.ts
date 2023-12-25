80790818e0c83abde96b18c7f21af03003168dc4
DININDU THARUKA KALUARACHCHI <dinidutharuka@gmail.com>
2023-12-25 13:56:40 +0530

    build

--- frontend/src/services/api-client.ts
+++ frontend/src/services/api-client.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-export const BASE_URL = 'http://127.0.0.1:8000'
+// http://hotelbackend.ebrandinginnovations.com
+
+// http://127.0.0.1:8000
+
+export const BASE_URL = 'http://hotelbackend.ebrandinginnovations.com'
 
 const axiosInstance =  axios.create({
     baseURL:`${BASE_URL}/api`,
